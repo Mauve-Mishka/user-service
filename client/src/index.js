f5e7331849c248947796b00df999fdc09b7ed247
Sam Cox <sjamcox@gmail.com>
2021-01-15 17:09:13 -0800

    Set up basic React app and container component

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+<<<<<<< HEAD
 import HostSection from './components/HostSection';
 
 ReactDOM.render(<HostSection />, document.getElementById('root'));
+=======
+import MorePlacesSection from './components/MorePlacesSection';
+
+ReactDOM.render(<MorePlacesSection />, document.getElementById('root'));
+>>>>>>> 3aba173... Set up basic React app and container component
